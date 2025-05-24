52df69970a1de1d0f9410f2e0b24e69b6045b84f
Gyanendra Singh <71885051+dev-gyanendra-singh@users.noreply.github.com>
2025-05-24 14:25:49 +0530

    Create Stack_Valid_Parenthesis.java

---
+++ Stack_Valid_Parenthesis.java
@@ -0,0 +1,24 @@
+
+public class Stack_Valid_Parenthesis {
+    public static void main(String[] args) {
+        System.out.println(new Stack_Valid_Parenthesis().isValid("([{}])"));
+    }
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char pop = stack.pop();
+
+                if (c == ')' && pop != '(' || pop != '{' && c == '}' || pop != '[' && c == ']') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
