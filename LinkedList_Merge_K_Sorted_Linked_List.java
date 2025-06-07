c95fe429694cfab7f854d50f8cc0cea0b17dfeac
Gyanendra Singh <71885051+dev-gyanendra-singh@users.noreply.github.com>
2025-06-08 00:12:31 +0530

    Create LinkedList_Merge_K_Sorted_Linked_List.java

---
+++ LinkedList_Merge_K_Sorted_Linked_List.java
@@ -0,0 +1,65 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class LinkedList_Merge_K_Sorted_Linked_List {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists == null || lists.length == 0) return null;
+        int interval = 1;
+        while (interval < lists.length) {
+            for (int i = 0; i + interval < lists.length; i += interval * 2) {
+                lists[i] = mergeTwoLists(lists[i], lists[i + interval]);
+            }
+            interval *= 2;
+        }
+        return lists[0];
+    }
+
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+        tail.next = (l1 != null) ? l1 : l2;
+        return dummy.next;
+    }
+
+    // Helper method to print a linked list
+    private static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val);
+            if (node.next != null) System.out.print(" -> ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    // Main method to run and test the merge
+    public static void main(String[] args) {
+        LinkedList_Merge_K_Sorted_Linked_List merger = new LinkedList_Merge_K_Sorted_Linked_List();
+
+        // Creating example linked lists: [1,4,5], [1,3,4], [2,6]
+        ListNode l1 = new ListNode(1, new ListNode(4, new ListNode(5)));
+        ListNode l2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        ListNode l3 = new ListNode(2, new ListNode(6));
+
+        ListNode[] lists = new ListNode[]{l1, l2, l3};
+
+        // Merge all lists
+        ListNode merged = merger.mergeKLists(lists);
+
+        // Print result
+        printList(merged);
+    }
+}
